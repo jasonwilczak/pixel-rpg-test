bb97069013762f51a2bd39aa1747fdb4d58f76fc
Jason Wilczak <jason.wilczak@gmail.com>
2022-11-30 08:53:26 -0500

    initial checkin

---
+++ Pixel RPG Test/Assets/Creator Kit - RPG/Scripts/Tiles/Isometric Rule Tile/IsometricRuleTile.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace UnityEngine
+{
+    public class IsometricRuleTile<T> : IsometricRuleTileOverride
+    {
+        public sealed override Type m_NeighborType { get { return typeof(T); } }
+    }
+
+    [Serializable]
+    [CreateAssetMenu(fileName = "New Isometric Rule Tile", menuName = "Tiles/Isometric Rule Tile")]
+    public class IsometricRuleTileOverride : RuleTile
+    {
+        // This has no differences with the RuleTile
+    }
+}
